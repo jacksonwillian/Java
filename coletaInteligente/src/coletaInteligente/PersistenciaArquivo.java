fdee0941534fc8b4d1176b064f19e2b0beb70298
Jackson Willian <jacksonwilliansilvaagostinho@gmail.com>
2019-04-04 12:48:42 -0300

    Classe conceito do trabalho
    Implementação de um classe do trabalho Lixeira Inteligente

---
+++ coletaInteligente/src/coletaInteligente/PersistenciaArquivo.java
@@ -0,0 +1,30 @@
+package coletaInteligente;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author Jackson Willian Silva Agostinho
+ */
+
+public class PersistenciaArquivo {
+       
+    public void salvaColetor(Coletor coletor) {
+        
+        FileWriter arq;
+        try {
+            arq = new FileWriter("/home/aluno/Downloads/Coletor.txt", true);        
+            PrintWriter gravarArq = new PrintWriter(arq);
+            gravarArq.append(coletor.getCodigo() + "," + coletor.getPlaca() + ","+ coletor.getMarca() + "," + coletor.getModelo() + "," + coletor.getAno() + "," + coletor.getCapacidade() + "," + coletor.getLatitude() + "," + coletor.getLongitude() + "\r\n");        
+            arq.close();   
+        } catch (IOException ex) {
+            Logger.getLogger(PersistenciaArquivo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+     
+    
+    
+}
